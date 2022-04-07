fc8118f25983d44d032a0f34031a710017b5ee76
Ankit Yadav <ankitfake913@gmail.com>
2022-04-07 22:55:37 +0530

    Second

--- app.js
+++ app.js
@@ -57,6 +57,6 @@ app.post("/",function(req,res){
     res.redirect("/");
 })
 
-app.listen(3000, function(){
+app.listen(process.env.PORT||3000, function(){
     console.log("listening on PORT 3000!");
 })
